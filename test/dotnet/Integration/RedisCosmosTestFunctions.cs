57ceddd414ff091da197823f3697ef202729cd16
Riley MacDonald <t-rmacdonald@microsoft.com>
2023-07-19 10:39:49 -0700

    Added basic testing for redis to cosmos pubsub function triggers

---
+++ test/dotnet/Integration/RedisCosmosTestFunctions.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
+{
+    internal class RedisCosmosTestFunctions
+    {
+        public const string localhostSetting = "redisLocalhost";
+        public const string cosmosDbConnectionSetting = "CosmosDBConnection";
+
+        public const string pubsubChannel = "testChannel";
+        public const string pubsubMultiple = "testChannel*";
+        public const string keyspaceChannel = "__keyspace@0__:testKey";
+        public const string keyspaceMultiple = "__keyspace@0__:testKey*";
+        public const string keyeventChannelSet = "__keyevent@0__:set";
+        public const string keyeventChannelAll = "__keyevent@0__:*";
+        public const string keyspaceChannelAll = "__keyspace@0__:*";
+        public const string allChannels = "*";
+
+
+        private static readonly IDatabase s_redisDb =
+            ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, localhostSetting)).GetDatabase();
+
+        private static readonly Lazy<IDatabaseAsync> s_readRedisDb = new Lazy<IDatabaseAsync>(() =>
+            ConnectionMultiplexer.ConnectAsync(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, localhostSetting)).Result.GetDatabase());
+
+        // PubSubWrite -- Tests
+        [FunctionName(nameof(SingleChannelToCosmos))]
+        public static async Task SingleChannelToCosmos(
+            [RedisPubSubTrigger(localhostSetting, pubsubChannel)] ChannelMessage pubSubMessage,
+             [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "PSContainerId",
+                Connection = cosmosDbConnectionSetting)]IAsyncCollector<PubSubData> cosmosOut,
+            ILogger logger)
+        {
+            //create a PubSubData object from the pubsub message
+            PubSubData redisData = new PubSubData(
+                id: Guid.NewGuid().ToString(),
+                channel: pubSubMessage.Channel,
+                message: pubSubMessage.Message,
+                timestamp: DateTime.UtcNow
+                );
+
+            //write the PubSubData object to Cosmos DB
+            await cosmosOut.AddAsync(redisData);
+            logger.LogInformation($"Message: \"{redisData.message}\" from Channel: \"{redisData.channel}\" stored in Cosmos DB container: \"{"PSContainerId"}\" with id: \"{redisData.id}\"");
+        }
+
+        [FunctionName(nameof(MultipleChannelsToCosmos))]
+        public static async Task MultipleChannelsToCosmos(
+            [RedisPubSubTrigger(localhostSetting, pubsubMultiple)] ChannelMessage pubSubMessage,
+             [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "PSContainerId",
+                Connection = cosmosDbConnectionSetting)]IAsyncCollector<PubSubData> cosmosOut,
+            ILogger logger)
+        {
+            //create a PubSubData object from the pubsub message
+            PubSubData redisData = new PubSubData(
+                id: Guid.NewGuid().ToString(),
+                channel: pubSubMessage.Channel,
+                message: pubSubMessage.Message,
+                timestamp: DateTime.UtcNow
+                );
+
+            //write the PubSubData object to Cosmos DB
+            await cosmosOut.AddAsync(redisData);
+            logger.LogInformation($"Message: \"{redisData.message}\" from Channel: \"{redisData.channel}\" stored in Cosmos DB container: \"{"PSContainerId"}\" with id: \"{redisData.id}\"");
+        }
+
+        [FunctionName(nameof(AllChannelsToCosmos))]
+        public static async Task AllChannelsToCosmos(
+            [RedisPubSubTrigger(localhostSetting, allChannels)] ChannelMessage pubSubMessage,
+             [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "PSContainerId",
+                Connection = cosmosDbConnectionSetting)]IAsyncCollector<PubSubData> cosmosOut,
+            ILogger logger)
+        {
+            //create a PubSubData object from the pubsub message
+            PubSubData redisData = new PubSubData(
+                id: Guid.NewGuid().ToString(),
+                channel: pubSubMessage.Channel,
+                message: pubSubMessage.Message,
+                timestamp: DateTime.UtcNow
+                );
+
+            //write the PubSubData object to Cosmos DB
+            await cosmosOut.AddAsync(redisData);
+            logger.LogInformation($"Message: \"{redisData.message}\" from Channel: \"{redisData.channel}\" stored in Cosmos DB container: \"{"PSContainerId"}\" with id: \"{redisData.id}\"");
+        }
+        
+        //write-through -- Tests
+        [FunctionName(nameof(WriteThrough))]
+        public static void WriteThrough(
+           [RedisPubSubTrigger(localhostSetting, "__keyevent@0__:set")] string newKey,
+           [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "ContainerId",
+                Connection = cosmosDbConnectionSetting)]out dynamic redisData,
+           ILogger logger)
+        {
+            //assign the data from Redis to a dynamic object that will be written to Cosmos DB
+            redisData = new RedisData(
+                id: Guid.NewGuid().ToString(),
+                key: newKey,
+                value: s_redisDb.StringGet(newKey),
+                timestamp: DateTime.UtcNow
+            );
+
+            logger.LogInformation($"Key: \"{newKey}\", Value: \"{redisData.value}\" addedd to Cosmos DB container: \"{"ContainerId"}\" at id: \"{redisData.id}\"");
+        }
+
+        //Write-Behind -- Tests
+        [FunctionName(nameof(WriteBehindAsync))]
+        public static async Task WriteBehindAsync(
+            [RedisPubSubTrigger(localhostSetting, "__keyevent@0__:set")] string newKey,
+            [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "ContainerId",
+                Connection = cosmosDbConnectionSetting)]IAsyncCollector<RedisData> cosmosOut,
+            ILogger logger)
+        {
+            //load data from Redis into a record
+            RedisData redisData = new RedisData(
+                id: Guid.NewGuid().ToString(),
+                key: newKey,
+                value: await s_redisDb.StringGetAsync(newKey),
+                timestamp: DateTime.UtcNow
+                );
+
+            //write the record to Cosmos DB
+            await cosmosOut.AddAsync(redisData);
+            logger.LogInformation($"Key: \"{newKey}\", Value: \"{redisData.value}\" added to Cosmos DB container: \"{"ContainerId"}\" at id: \"{redisData.id}\"");
+        }
+
+        //Write-Around -- Tests
+        [FunctionName(nameof(WriteAroundAsync))]
+        public static async Task WriteAroundAsync([CosmosDBTrigger(
+            databaseName: "DatabaseId",
+            containerName: "ContainerId",
+            Connection = cosmosDbConnectionSetting,
+            LeaseContainerName = "leases", LeaseContainerPrefix = "Write-Around-")]IReadOnlyList<RedisData> input,
+            ILogger logger)
+        {
+            //if the list is empty, return
+            if (input == null || input.Count <= 0) return;
+
+            //for each item upladed to cosmos, write it to Redis
+            foreach (var document in input)
+            {
+                //if the key/value pair is already in Redis, throw an exception
+                if (await s_redisDb.StringGetAsync(document.key) == document.value)
+                {
+                    throw new Exception($"ERROR: Key: \"{document.key}\", Value: \"{document.value}\" pair is already in Azure Redis Cache.");
+                }
+                //Write the key/value pair to Redis
+                await s_redisDb.StringSetAsync(document.key, document.value);
+                logger.LogInformation($"Key: \"{document.key}\", Value: \"{document.value}\" added to Redis.");
+            }
+        }
+
+        //Read-Through -- Tests
+        [FunctionName(nameof(ReadThroughAsync))]
+        public static async Task ReadThroughAsync(
+            [RedisPubSubTrigger(localhostSetting, "__keyevent@0__:keymiss")] string missedkey,
+            [CosmosDB(
+                databaseName: "DatabaseId",
+                containerName: "ContainerId",
+                Connection = cosmosDbConnectionSetting)]CosmosClient client,
+            ILogger logger)
+        {
+            //get the Cosmos DB database and the container to read from
+            Container cosmosContainer = client.GetContainer("DatabaseId", "ContainerId");
+            var queryable = cosmosContainer.GetItemLinqQueryable<RedisData>();
+
+            //get all entries in the container that contain the missed key
+            using FeedIterator<RedisData> feed = queryable
+                .Where(p => p.key == missedkey)
+                .OrderByDescending(p => p.timestamp)
+                .ToFeedIterator();
+            var response = await feed.ReadNextAsync();
+
+            //if the key is found in Cosmos DB, add  the most recently updated to Redis
+            var item = response.FirstOrDefault(defaultValue: null);
+            if (item != null)
+            {
+                await s_readRedisDb.Value.StringSetAsync(item.key, item.value);
+                logger.LogInformation($"Key: \"{item.key}\", Value: \"{item.value}\" added to Redis.");
+            }
+            else
+            {
+                //if the key isnt found in Cosmos DB, throw an exception
+                throw new Exception($"ERROR: Key: \"{missedkey}\" not found in Redis or Cosmos DB. Try adding the Key-Value pair to Redis or Cosmos DB.");
+            }
+        }
+    }
+}
---
+++ test/dotnet/Integration/RedisCosmosTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
+{
+    [Collection("PubSubTriggerTests")]
+    public class RedisCosmosTests
+    {
+
+        [Theory]
+        [InlineData(nameof(RedisCosmosTestFunctions.SingleChannelToCosmos), RedisCosmosTestFunctions.pubsubChannel, "testValue single")]
+        [InlineData(nameof(RedisCosmosTestFunctions.SingleChannelToCosmos), RedisCosmosTestFunctions.pubsubChannel, "testValue multi")]
+        [InlineData(nameof(RedisCosmosTestFunctions.MultipleChannelsToCosmos), RedisCosmosTestFunctions.pubsubChannel + "suffix", "testSuffix multi")]
+        [InlineData(nameof(RedisCosmosTestFunctions.AllChannelsToCosmos), RedisCosmosTestFunctions.pubsubChannel + "suffix", "testSuffix all")]
+        [InlineData(nameof(RedisCosmosTestFunctions.AllChannelsToCosmos), "prefix" + RedisCosmosTestFunctions.pubsubChannel, "testPrefix all")]
+        [InlineData(nameof(RedisCosmosTestFunctions.AllChannelsToCosmos), "separate", "testSeparate all")]
+        public async void PubSubMessageWrite_SuccessfullyWritesToCosmos(string functionName, string channel, string message)
+        {
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.localhostSetting)))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7079))
+            {
+                ISubscriber subscriber = multiplexer.GetSubscriber();
+
+                subscriber.Publish(channel, message);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                await multiplexer.CloseAsync();
+                functionsProcess.Kill();
+            };
+
+            string cosmosMessage = null;
+            using (CosmosClient client = new CosmosClient(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.cosmosDbConnectionSetting)))
+            {
+                var db = client.GetContainer("DatabaseId", "PSContainerId");
+                var queryable = db.GetItemLinqQueryable<PubSubData>();
+
+                //get all entries in the container that contain the missed key
+                using FeedIterator<PubSubData> feed = queryable
+                    .Where(p => p.channel == channel)
+                    .OrderByDescending(p => p.timestamp)
+                    .ToFeedIterator();
+                var response = await feed.ReadNextAsync();
+                var item = response.FirstOrDefault(defaultValue: null);
+                cosmosMessage = item?.message;
+            }
+
+            Assert.True(message == cosmosMessage, $"Expected \"{message}\" but got \"{cosmosMessage}\"");
+            IntegrationTestHelpers.ClearDataFromCosmosDb("DatabaseId", "PSContainerId");
+        }
+
+
+        [Theory]
+        [InlineData(nameof(RedisCosmosTestFunctions.WriteThrough), "testKey-1", "testValue1")]
+        [InlineData(nameof(RedisCosmosTestFunctions.WriteBehindAsync), "testKey-2", "testValue2")]
+        public async void RedisToCosmos_SuccessfullyWritesToCosmos(string functionName, string key, string value)
+        { 
+            string keyFromCosmos = null;
+            string valueFromCosmos = null;
+            
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.localhostSetting)))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7079))
+            {
+                var redisDb = multiplexer.GetDatabase();
+                await redisDb.StringSetAsync(key, value);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                
+                using (CosmosClient client = new CosmosClient(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.cosmosDbConnectionSetting)))
+                {
+                    var cosmosDb = client.GetContainer("DatabaseId", "ContainerId");
+                    var queryable = cosmosDb.GetItemLinqQueryable<RedisData>();
+
+                //get all entries in the container that contain the missed key
+                    using FeedIterator<RedisData> feed = queryable
+                        .Where(p => p.key == key)
+                        .OrderByDescending(p => p.timestamp)
+                        .ToFeedIterator();
+                    var response = await feed.ReadNextAsync();
+                    await Task.Delay(TimeSpan.FromSeconds(3));
+
+                    var item = response.FirstOrDefault(defaultValue: null);
+
+                    keyFromCosmos = item?.key;
+                    valueFromCosmos = item?.value;
+                };
+
+                await redisDb.KeyDeleteAsync(key);
+                functionsProcess.Kill();
+            };
+
+            Assert.True(keyFromCosmos == key, $"Expected \"{key}\" but got \"{keyFromCosmos}\"");
+            Assert.True(valueFromCosmos == value, $"Expected \"{value}\" but got \"{valueFromCosmos}\"");
+            IntegrationTestHelpers.ClearDataFromCosmosDb("DatabaseId", "ContainerId");
+        }
+
+        [Fact]
+        public async void WriteAround_SuccessfullyWritesToRedis()
+        {
+            string functionName = nameof(RedisCosmosTestFunctions.WriteAroundAsync);
+            using (CosmosClient client = new CosmosClient(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.cosmosDbConnectionSetting)))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7081))
+            {
+                Container cosmosContainer = client.GetContainer("DatabaseId", "ContainerId");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                RedisData redisData = new RedisData(
+                    id: Guid.NewGuid().ToString(),
+                    key: "cosmosKey",
+                    value: "cosmosValue",
+                    timestamp: DateTime.UtcNow
+                );
+                
+                await cosmosContainer.CreateItemAsync(redisData);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                client.Dispose();
+                functionsProcess.Kill();
+
+            }
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.localhostSetting)))
+            {
+                var redisValue = await multiplexer.GetDatabase().StringGetAsync("cosmosKey");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                Assert.Equal("cosmosValue", redisValue);
+                //await multiplexer.GetDatabase().KeyDeleteAsync("cosmosKey");
+                // await Task.Delay(TimeSpan.FromSeconds(3));
+                await multiplexer.CloseAsync();
+            }
+            IntegrationTestHelpers.ClearDataFromCosmosDb("DatabaseId", "ContainerId");
+        }
+
+        [Fact]
+        public async void ReadThrough_SuccessfullyWritesToRedis()
+        {
+            string functionName = nameof(RedisCosmosTestFunctions.ReadThroughAsync);
+            using (CosmosClient client = new CosmosClient(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.cosmosDbConnectionSetting)))
+            {
+                Container cosmosContainer = client.GetContainer("DatabaseId", "ContainerId");
+                RedisData redisData = new RedisData(
+                    id: Guid.NewGuid().ToString(),
+                    key: "cosmosKey1",
+                    value: "cosmosValue1",
+                    timestamp: DateTime.UtcNow
+                );
+                await cosmosContainer.UpsertItemAsync(redisData);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                client.Dispose();
+            }
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisCosmosTestFunctions.localhostSetting)))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7082))
+            {
+                var redisValue = await multiplexer.GetDatabase().StringGetAsync("cosmosKey1");
+                Assert.True(redisValue.IsNull, userMessage: "Key already in Redis Cache, test failed");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                redisValue = await multiplexer.GetDatabase().StringGetAsync("cosmosKey1");
+                
+                await Task.Delay(TimeSpan.FromSeconds(3));
+
+                Assert.Equal("cosmosValue1", redisValue);
+                await multiplexer.GetDatabase().KeyDeleteAsync("cosmosKey1");
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                await multiplexer.CloseAsync();
+            }
+            IntegrationTestHelpers.ClearDataFromCosmosDb("DatabaseId", "ContainerId");
+        }
+    }
+}
