016607bd1068d6f3ff9307c4053885890fa120c8
Riley MacDonald <t-rmacdonald@microsoft.com>
2023-08-10 10:38:23 -0700

    rebased from upstream branch

--- samples/CosmosDBIntegration/WriteBehindSamples/StreamSample.cs
+++ samples/CosmosDBIntegration/WriteBehindSamples/StreamSample.cs
@@ -3,10 +3,6 @@ using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Azure.WebJobs.Extensions.Redis.Samples.Models;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
